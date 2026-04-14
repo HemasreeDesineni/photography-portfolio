bfbd879621f9c738ab3e0f3847fe32ac6af5fd6a
HemasreeDesineni <hemasree.desineni@gmail.com>
2026-04-14 22:40:39 +0530

    page 1 fix
    page 1 title fix

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -18,7 +18,7 @@ export default function Hero() {
       <div className="absolute top-0 w-full h-[70px] bg-[#7a0000] z-10" />
 
       {/* TITLE (OVERLAPPING RED + IMAGE) */}
-      <div className="absolute top-[45px] w-full text-center text-white font-serif text-4xl z-30">
+      <div className="absolute top-[45px] w-full text-center text-white font-serif text-4xl z-30 drop-shadow-[0_2px_6px_rgba(0,0,0,0.8)]">
         Goutham Photography
         <span className="absolute right-10 text-xl top-1 -translate-y-1/2">
           Contact
