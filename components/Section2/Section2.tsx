194d7940a2413e73237451dd86f12e02195ff19a
HemasreeDesineni <hemasree.desineni@gmail.com>
2026-04-15 15:48:24 +0530

    page 2 text adjustments
    page 2 text adjustments

--- components/Section2/Section2.tsx
+++ components/Section2/Section2.tsx
@@ -12,6 +12,7 @@ export default function Section2() {
     offset: ["start end", "end start"],
   });
 
+  // Background animation
   const blur = useTransform(scrollYProgress, [0, 0.5, 1], [20, 0, 20]);
   const scale = useTransform(scrollYProgress, [0, 0.5, 1], [1.05, 1, 1.05]);
   const blurValue = useTransform(blur, (b) => `blur(${b}px)`);
@@ -40,55 +41,61 @@ export default function Section2() {
       </motion.div>
 
       {/* YELLOW OVERLAY */}
-      <div className="absolute inset-0 bg-[#8a5a2b]/70 z-10" />
+      <div className="absolute inset-0 bg-[#8a5a2b]/35 z-10" />
 
       {/* TOP STRIP */}
-      <div className="absolute top-0 w-full h-[35px] bg-[#b88a52] z-20" />
+      <div className="absolute top-0 w-full h-[10px] bg-[#b88a52] z-20" />
 
       {/* BOTTOM STRIP */}
-      <div className="absolute bottom-0 w-full h-[30px] bg-[#b88a52] z-20" />
+      <div className="absolute bottom-0 w-full h-[10px] bg-[#b88a52] z-20" />
 
       {/* CONTACT */}
-      <div className="absolute top-[50px] right-10 z-30 text-white text-lg font-[var(--font-playfair)]">
+      <div className="absolute top-[25px] right-10 z-30 text-white text-lg font-[var(--font-playfair)]">
         Contact
       </div>
 
-      {/* CONTENT (ABSOLUTE LAYOUT) */}
+      {/* CONTENT */}
       <motion.div
         style={{ opacity: contentOpacity }}
-        className="absolute inset-0 z-30"
+        className="absolute inset-0 z-50"
       >
 
         {/* TITLE */}
-        <div className="absolute top-[80px] left-1/2 -translate-x-1/2">
+        <div className="absolute top-[-1px] left-1/2 -translate-x-1/2">
           <Image
             src="/images/page2-title.png"
             alt="title"
-            width={700}
-            height={200}
+            width={1000}
+            height={300}
             priority
+            className="w-[90vw] max-w-[1800px] h-auto"
+            style={{ mixBlendMode: "normal" }}
           />
         </div>
 
         {/* I COMPOSE */}
-        <div className="absolute top-[160px] left-1/2 -translate-x-1/2">
+        <div className="absolute top-[-11px] left-1/2 -translate-x-1/2">
           <Image
             src="/images/page2-icompose-text.png"
             alt="compose"
-            width={800}
-            height={250}
+            width={1200}
+            height={350}
             priority
+            className="w-[100vw] max-w-[1200px] h-auto"
+            style={{ mixBlendMode: "normal" }}
           />
         </div>
 
         {/* WHITE TEXT */}
-        <div className="absolute top-[320px] left-1/2 -translate-x-1/2">
+        <div className="absolute top-[20px] left-1/2 -translate-x-1/2">
           <Image
             src="/images/page2-white-text.png"
             alt="text"
-            width={900}
-            height={400}
+            width={1300}
+            height={600}
             priority
+            className="w-[95vw] max-w-[1100px] h-auto"
+            style={{ mixBlendMode: "normal" }}
           />
         </div>
 
@@ -97,7 +104,7 @@ export default function Section2() {
       {/* ARROW */}
       <button
         onClick={scrollToTop}
-        className="absolute bottom-[40px] left-6 z-40 text-white text-5xl"
+        className="absolute bottom-[40px] left-6 z-40 text-white text-5xl hover:scale-110 transition"
       >
         ↑
       </button>
