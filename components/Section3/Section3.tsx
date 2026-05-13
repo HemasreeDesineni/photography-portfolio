024dde3a0106ba3f7371d27d81faba41e31544b0
HemasreeDesineni <hemasree.desineni@gmail.com>
2026-05-13 16:00:23 +0530

    section3 animations fix

--- components/Section3/Section3.tsx
+++ components/Section3/Section3.tsx
@@ -140,22 +140,21 @@ export default function Section3({
               <motion.div
                 initial={{
                   opacity: 0,
-                  y: -50,
-                  filter: "blur(28px)",
+                  filter: "blur(24px)",
                 }}
                 animate={{
                   opacity: 1,
-                  y: 0,
                   filter: "blur(0px)",
                 }}
                 transition={{
-                  duration: 2.2,
+                  duration: 2,
+                  delay: 0.25,
                   ease: cinematicEase,
                 }}
                 className="
                 pointer-events-none
                 absolute
-                left-[-210px]
+                left-[53%]
                 top-[-240px]
                 z-20
                 h-[750px]
